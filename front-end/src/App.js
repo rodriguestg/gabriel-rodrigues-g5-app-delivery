b40b7a5d933cca78a27f5e2dd43ecebe318babfb
Rikelmy Lopes <rikelmylopes899@gmail.com>
2023-01-23 17:21:46 -0300

    navbar feito

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <Switch>
-      <Route path="/customer/products" />
+      <Route path="/customer/products" component={ NavBar } />
       <Route path="/customer/checkout" />
       <Route path="/customer/orders/:id" />
       <Route path="/customer/orders" />
---
+++ front-end/src/components/NavBar.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+// import getUserLocalStorage from '../utils/userLocalStorage';
+
+export default function NavBar() {
+  const { name, role } = JSON.parse(localStorage.getItem('user'));
+  const nameTag = (
+    <p data-testid="customer_products__element-navbar-user-full-name">
+      { name }
+    </p>
+  );
+  const logoutTag = (
+    <Link
+      to="/"
+      data-testid="customer_products__element-navbar-link-orders"
+    >
+      Logout
+    </Link>
+  );
+
+  if (role === 'customer') {
+    return (
+      <div>
+        <Link
+          to="/customer/products"
+          data-testid="customer_products__element-navbar-link-products"
+        >
+          Produtos
+        </Link>
+        <Link
+          to="/customer/checkout"
+          data-testid="customer_products__element-navbar-link-orders"
+        >
+          Meus Pedidos
+        </Link>
+        {nameTag}
+        {logoutTag}
+      </div>
+    );
+  } if (role === 'seller') {
+    return (
+      <div>
+        <Link
+          to="/customer/checkout"
+          data-testid="customer_products__element-navbar-link-orders"
+        >
+          Meus Pedidos
+        </Link>
+        {nameTag}
+        {logoutTag}
+      </div>
+    );
+  } if (role === 'administrator') {
+    return (
+      <div>
+        <Link
+          to="/admin/manage"
+          data-testid="customer_products__element-navbar-link-orders"
+        >
+          Gerenciar Usuarios
+        </Link>
+        {nameTag}
+        {logoutTag}
+      </div>
+    );
+  }
+}
