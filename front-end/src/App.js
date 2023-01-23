acf2163b5d1c606fc62ce9073ab439b57f0b728d
Danubio Rafael <danubio.rafaeel@gmail.com>
2023-01-23 19:13:36 -0300

    feat: Req-1 e Req-2

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import NavBar from './components/NavBar';
+import Login from './pages/Login';
 
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
       <Route path="/seller/orders/:id" />
       <Route path="/seller/orders" />
       <Route path="/admin/manage" />
-      <Route path="/" />
+      <Route path="/" component={ Login } />
     </Switch>
   );
 }
---
+++ front-end/src/pages/Login.js
@@ -0,0 +1,43 @@
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+
+export default function Login() {
+  const history = useHistory();
+  useEffect(() => {
+    if (history.location.pathname === '/') history.push('/login');
+  }, []);
+
+  return (
+    <div className="container-login">
+      <form className="login">
+        <input
+          data-testid="common_login__input-email"
+          type="email"
+          placeholder="email"
+          id="email"
+        />
+        <input
+          data-testid="common_login__input-password"
+          type="password"
+          placeholder="password"
+          id="password"
+        />
+        <button
+          data-testid="common_login__button-login"
+          type="button"
+          onClick={ () => {
+            localStorage.setItem();
+          } }
+        >
+          Login
+        </button>
+        <button
+          data-testid="common_login__button-register"
+          type="button"
+        >
+          Ainda não tenho conta
+        </button>
+      </form>
+    </div>
+  );
+}
