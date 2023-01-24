ad4ecef842fcdec3c9e8d6f97ea432590e7c712a
Danubio Rafael <danubio.rafaeel@gmail.com>
2023-01-24 18:23:06 -0300

    feat - Req-28

---
+++ front-end/src/components/SaleCard.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function SaleCard({ sale }) {
+  const { id,
+    status,
+    deliveryAddress,
+    deliveryNumber,
+    saleDate,
+    totalPrice } = sale;
+
+  const date = new Date(saleDate).getDate();
+
+  return (
+    <div>
+      <p data-testid={ `seller_orders__element-order-id-${id}` }>{id}</p>
+      <p data-testid={ `seller_orders__element-delivery-status-${id}` }>{status}</p>
+      <p data-testid={ `seller_orders__element-card-address-${id}` }>
+        {`${deliveryAddress}, ${deliveryNumber}`}
+      </p>
+      <p data-testid={ `seller_orders__element-order-date-${id}` }>{date}</p>
+      <p data-testid={ `seller_orders__element-card-price-${id}` }>{totalPrice}</p>
+    </div>
+  );
+}
+
+SaleCard.propTypes = {
+  sale: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    userId: PropTypes.number.isRequired,
+    sellerId: PropTypes.number.isRequired,
+    totalPrice: PropTypes.number.isRequired,
+    deliveryAddress: PropTypes.string.isRequired,
+    deliveryNumber: PropTypes.string.isRequired,
+    saleDate: PropTypes.string.isRequired,
+    status: PropTypes.string.isRequired,
+    user_id: PropTypes.number.isRequired,
+    seller_id: PropTypes.number.isRequired,
+  }).isRequired,
+};
--- front-end/src/pages/SellerPage.jsx
+++ front-end/src/pages/SellerPage.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import NavBar from '../components/NavBar';
-import OrderCard from '../components/OrderCard';
+import SaleCard from '../components/SaleCard';
+import fetchUtil from '../utils/fetchUtil';
 
 export default function SellerPage() {
+  const [sales, setSales] = useState([]);
+
+  useEffect(() => {
+    const getSales = async () => {
+      const response = await fetchUtil.fetchWithoutBody('/sales', 'GET');
+
+      if (Array.isArray(response)) setSales(response);
+    };
+
+    getSales();
+  }, []);
+
   return (
     <div>
       <NavBar />
       <main>
-        <OrderCard />
+        { sales.length && sales.map((sale) => <SaleCard sale={ sale } key={ sale.id } />)}
+
       </main>
     </div>
   );
