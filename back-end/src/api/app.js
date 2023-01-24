f5ec7c38dd6606bd6b0d31868b6e6810b4223015
Rikelmy Lopes <rikelmylopes899@gmail.com>
2023-01-24 16:48:52 -0300

    erros de lint

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 const productRouter = require('./routes/productRouter');
-const path = require('path')
 
 const LoginController = require('./controller/login.controller');
 
