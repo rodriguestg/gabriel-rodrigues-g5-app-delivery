6a8c00a4ee2544756f95b2e0b2c80cf6280732a5
Rikelmy Lopes <rikelmylopes899@gmail.com>
2023-01-26 19:35:30 -0300

    testes de integracao na rota /products

--- back-end/src/tests/integration/login.mock.js
+++ back-end/src/tests/integration/login.mock.js
@@ -1,4 +1,4 @@
-const expectedLoginResult = {
+const expectedOutput = {
     "name": "Cliente Zé Birita",
     "email": "zebirita@email.com",
     "token": "eyJhbGciOiJIUzI1NiJ9.Mw.OW7ZXw5IGgg6GVIvvSao2jVyQcDLP2Ld7v9uaYr_b7g",
@@ -13,4 +13,4 @@ const findOneMock = {
       role: 'customer'
   }
 
-  module.exports = { findOneMock, expectedLoginResult }
+  module.exports = { findOneMock, expectedOutput }
