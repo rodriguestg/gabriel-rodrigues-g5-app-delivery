fc946251b488fa482d9ebc98829c3759b6c94739
Rikelmy Lopes <rikelmylopes899@gmail.com>
2023-01-27 18:37:20 -0300

    mais testes na Rota /sales

--- back-end/src/tests/integration/sale.mock.js
+++ back-end/src/tests/integration/sale.mock.js
@@ -1,4 +1,6 @@
-const findOneMock = {
+// nome de variável: rotaDaApi_Mock ou Expected
+
+const salesUserEmail_FindOne = {
     id: 3,
     name: 'Cliente Zé Birita',
     email: 'zebirita@email.com',
@@ -6,7 +8,7 @@ const findOneMock = {
     role: 'customer'
   }
 
-const findAllMock = [ 
+  const salesUserEmail_FindAll = [ 
     {
         id: 1,
         userId: 3,
@@ -42,7 +44,7 @@ const findAllMock = [
       },
   ]
 
-const expectedOutput = [
+const salesUserEmail_Expected = [
     {
       "id": 1,
       "userId": 3,
@@ -78,4 +80,131 @@ const expectedOutput = [
     }
   ]
 
-module.exports = { findOneMock, findAllMock, expectedOutput }
+const salesDetailsSaleId_FindOne = {
+    id: 1,
+    userId: 3,
+    sellerId: 1,
+    totalPrice: '100.00',
+    deliveryAddress: 'Rua Tal',
+    deliveryNumber: '23',
+    saleDate: '2023-01-01T00:00:00.000Z',
+    status: 'pending',
+    seller_id: 1,
+    products: [],
+    users: {
+      id: 3,
+      name: 'Cliente Zé Birita',
+      email: 'zebirita@email.com',
+      password: '1c37466c159755ce1fa181bd247cb925',
+      role: 'customer'
+    },
+    seller: {
+      id: 1,
+      name: 'Delivery App Admin',
+      email: 'adm@deliveryapp.com',
+      password: 'a4c86edecc5aee06eff8fdeda69e0d04',
+      role: 'administrator'
+    },
+  };
+
+const salesDetailsSaleId_Expected = {
+    "id": 1,
+    "userId": 3,
+    "sellerId": 1,
+    "totalPrice": "100.00",
+    "deliveryAddress": "Rua Tal",
+    "deliveryNumber": "23",
+    "saleDate": "2023-01-01T00:00:00.000Z",
+    "status": "pending",
+    "seller_id": 1,
+    "products": [],
+    "users": {
+      "id": 3,
+      "name": "Cliente Zé Birita",
+      "email": "zebirita@email.com",
+      "password": "1c37466c159755ce1fa181bd247cb925",
+      "role": "customer"
+    },
+    "seller": {
+      "id": 1,
+      "name": "Delivery App Admin",
+      "email": "adm@deliveryapp.com",
+      "password": "a4c86edecc5aee06eff8fdeda69e0d04",
+      "role": "administrator"
+    }
+  }
+
+const sales_FindAll = [
+     {
+        id: 1,
+        userId: 3,
+        sellerId: 1,
+        totalPrice: '100.00',
+        deliveryAddress: 'Rua Tal',
+        deliveryNumber: '23',
+        saleDate: '2023-01-01T00:00:00.000Z',
+        status: 'pending',
+        seller_id: 1
+      },
+     {
+        id: 2,
+        userId: 3,
+        sellerId: 1,
+        totalPrice: '50.00',
+        deliveryAddress: 'Rua Ola Mundo',
+        deliveryNumber: '295',
+        saleDate: '2023-01-01T00:00:00.000Z',
+        status: 'pending',
+        seller_id: 1
+      },
+     {
+        id: 3,
+        userId: 3,
+        sellerId: 1,
+        totalPrice: '25.00',
+        deliveryAddress: 'Av Tancredo',
+        deliveryNumber: '100',
+        saleDate: '2023-01-01T00:00:00.000Z',
+        status: 'pending',
+        seller_id: 1
+      },
+  ]
+
+const sales_Expected = [
+  {
+    "id": 1,
+    "userId": 3,
+    "sellerId": 1,
+    "totalPrice": "100.00",
+    "deliveryAddress": "Rua Tal",
+    "deliveryNumber": "23",
+    "saleDate": "2023-01-01T00:00:00.000Z",
+    "status": "pending",
+    "seller_id": 1
+  },
+  {
+    "id": 2,
+    "userId": 3,
+    "sellerId": 1,
+    "totalPrice": "50.00",
+    "deliveryAddress": "Rua Ola Mundo",
+    "deliveryNumber": "295",
+    "saleDate": "2023-01-01T00:00:00.000Z",
+    "status": "pending",
+    "seller_id": 1
+  },
+  {
+    "id": 3,
+    "userId": 3,
+    "sellerId": 1,
+    "totalPrice": "25.00",
+    "deliveryAddress": "Av Tancredo",
+    "deliveryNumber": "100",
+    "saleDate": "2023-01-01T00:00:00.000Z",
+    "status": "pending",
+    "seller_id": 1
+  }
+]
+
+module.exports = { salesUserEmail_FindOne, salesUserEmail_FindAll, salesUserEmail_Expected,
+   salesDetailsSaleId_FindOne, salesDetailsSaleId_Expected, sales_FindAll, sales_Expected }
--- back-end/src/tests/integration/sale.test.js
+++ back-end/src/tests/integration/sale.test.js
@@ -4,7 +4,9 @@ const chaiHttp = require('chai-http')
 const app =  require('../../api/app')
 const mocha = require('mocha')
 const { Sale, User } = require('../../database/models')
-const { expectedOutput, findAllMock, findOneMock } = require('./sale.mock')
+const jwt = require('jsonwebtoken')
+const { salesUserEmail_FindOne, salesUserEmail_FindAll, salesUserEmail_Expected , salesDetailsSaleId_FindOne, 
+    salesDetailsSaleId_Expected, sales_FindAll, sales_Expected  } = require('./sale.mock')
 
 chai.use(chaiHttp)
 
@@ -15,12 +17,12 @@ describe('Testa a Rota GET /sales/user/:email', () => {
     let chaiHttpResponse;
 
     it('Testa se retorna todos os pedidos de um Usuário com SUCESSO', async () => {
-        sinon.stub(User, 'findOne').resolves(findOneMock)
-        sinon.stub(Sale, 'findAll').resolves(findAllMock)
+        sinon.stub(User, 'findOne').resolves(salesUserEmail_FindOne)
+        sinon.stub(Sale, 'findAll').resolves(salesUserEmail_FindAll)
         chaiHttpResponse = await chai.request(app).get(`/sales/user/${'zebirita@email.com'}`)
 
         expect(chaiHttpResponse).status(200)
-        expect(chaiHttpResponse.body).to.be.deep.equal(expectedOutput)
+        expect(chaiHttpResponse.body).to.be.deep.equal(salesUserEmail_Expected)
     })
 
     it('Testa se retorna 400 caso o email não seja passado', async () => {
@@ -28,5 +30,29 @@ describe('Testa a Rota GET /sales/user/:email', () => {
 
         expect(chaiHttpResponse).status(400)
         expect(chaiHttpResponse.body).to.be.deep.equal({ message: 'Email not sent'})
+    })
+
+    describe('Testa a Rota GET /sales/details/:saleId', () => {
+        afterEach(sinon.restore)
+        it('Testa se retorna a venda por Id com SUCESSO', async () => {
+            sinon.stub(Sale, 'findOne').resolves(salesDetailsSaleId_FindOne)
+            chaiHttpResponse = await chai.request(app).get(`/sales/details/${1}`)
+
+            expect(chaiHttpResponse).status(200)
+            expect(chaiHttpResponse.body).to.be.deep.equal(salesDetailsSaleId_Expected)
+        })
+    })
+
+    describe('Testa a Rota GET /sales', () => {
+        it('Testa se torna todos os pedidos do Vendedor com SUCESSO', async () => {
+            const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.MQ.4lwf4bp8So6V6exIb8i_y2u_QtTH5eRdQvtV2DcImF8'
+            sinon.stub(Sale, 'findAll').resolves(sales_FindAll)
+            sinon.stub(jwt, 'verify').resolves(1)
+            chaiHttpResponse = await chai.request(app).get('/sales')
+            .set('Authorization', token)
+
+            expect(chaiHttpResponse).status(200)
+            expect(chaiHttpResponse.body).to.be.deep.equal(sales_Expected)
+        })
     })
 })
