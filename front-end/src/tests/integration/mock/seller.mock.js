0dc5f76916d5966f03483842894111543dfadaae
Danubio Rafael <danubio.rafaeel@gmail.com>
2023-01-30 16:21:48 -0300

    fix - lint

--- front-end/src/tests/integration/mock/seller.mock.js
+++ front-end/src/tests/integration/mock/seller.mock.js
@@ -1,4 +1,45 @@
 const deliveryAddress = 'Xavier Alameda';
+const saleDate = '2023-01-26T22:54:38.000Z';
+
+const products = [
+  {
+    id: 2,
+    name: 'Heineken 600ml',
+    price: '7.50',
+    urlImage: 'http://localhost:3001/images/heineken_600ml.jpg',
+    SaleProduct: {
+      saleId: 1,
+      productId: 2,
+      quantity: 3,
+    },
+  },
+  {
+    id: 4,
+    name: 'Brahma 600ml',
+    price: '7.50',
+    urlImage: 'http://localhost:3001/images/brahma_600ml.jpg',
+    SaleProduct: {
+      saleId: 1,
+      productId: 4,
+      quantity: 3,
+    },
+  },
+];
+
+const users = {
+  id: 3,
+  name: 'Cliente Zé Birita',
+  email: 'zebirita@email.com',
+  password: '1c37466c159755ce1fa181bd247cb925',
+  role: 'customer',
+};
+const seller = {
+  id: 2,
+  name: 'Fulana Pereira',
+  email: 'fulana@deliveryapp.com',
+  password: '3c28d2b0881bf46457a853e0b07531c6',
+  role: 'seller',
+};
 
 const mockGetSalesOne = [{
   id: 1,
@@ -18,47 +59,12 @@ const mockOneSalePendente = {
   totalPrice: '63.14',
   deliveryAddress,
   deliveryNumber: '609',
-  saleDate: '2023-01-26T22:54:38.000Z',
+  saleDate,
   status: 'Pendente',
   seller_id: 2,
-  products: [
-    {
-      id: 2,
-      name: 'Heineken 600ml',
-      price: '7.50',
-      urlImage: 'http://localhost:3001/images/heineken_600ml.jpg',
-      SaleProduct: {
-        saleId: 1,
-        productId: 2,
-        quantity: 3,
-      },
-    },
-    {
-      id: 4,
-      name: 'Brahma 600ml',
-      price: '7.50',
-      urlImage: 'http://localhost:3001/images/brahma_600ml.jpg',
-      SaleProduct: {
-        saleId: 1,
-        productId: 4,
-        quantity: 3,
-      },
-    },
-  ],
-  users: {
-    id: 3,
-    name: 'Cliente Zé Birita',
-    email: 'zebirita@email.com',
-    password: '1c37466c159755ce1fa181bd247cb925',
-    role: 'customer',
-  },
-  seller: {
-    id: 2,
-    name: 'Fulana Pereira',
-    email: 'fulana@deliveryapp.com',
-    password: '3c28d2b0881bf46457a853e0b07531c6',
-    role: 'seller',
-  },
+  products,
+  users,
+  seller,
 };
 
 const mockOneSalePreparando = {
@@ -68,47 +74,12 @@ const mockOneSalePreparando = {
   totalPrice: '63.14',
   deliveryAddress,
   deliveryNumber: '609',
-  saleDate: '2023-01-26T22:54:38.000Z',
+  saleDate,
   status: 'Preparando',
   seller_id: 2,
-  products: [
-    {
-      id: 2,
-      name: 'Heineken 600ml',
-      price: '7.50',
-      urlImage: 'http://localhost:3001/images/heineken_600ml.jpg',
-      SaleProduct: {
-        saleId: 1,
-        productId: 2,
-        quantity: 3,
-      },
-    },
-    {
-      id: 4,
-      name: 'Brahma 600ml',
-      price: '7.50',
-      urlImage: 'http://localhost:3001/images/brahma_600ml.jpg',
-      SaleProduct: {
-        saleId: 1,
-        productId: 4,
-        quantity: 3,
-      },
-    },
-  ],
-  users: {
-    id: 3,
-    name: 'Cliente Zé Birita',
-    email: 'zebirita@email.com',
-    password: '1c37466c159755ce1fa181bd247cb925',
-    role: 'customer',
-  },
-  seller: {
-    id: 2,
-    name: 'Fulana Pereira',
-    email: 'fulana@deliveryapp.com',
-    password: '3c28d2b0881bf46457a853e0b07531c6',
-    role: 'seller',
-  },
+  products,
+  users,
+  seller,
 };
 
 const mockOneSaleEmTrânsito = {
@@ -118,47 +89,12 @@ const mockOneSaleEmTrânsito = {
   totalPrice: '63.14',
   deliveryAddress,
   deliveryNumber: '609',
-  saleDate: '2023-01-26T22:54:38.000Z',
+  saleDate,
   status: 'Em Trânsito',
   seller_id: 2,
-  products: [
-    {
-      id: 2,
-      name: 'Heineken 600ml',
-      price: '7.50',
-      urlImage: 'http://localhost:3001/images/heineken_600ml.jpg',
-      SaleProduct: {
-        saleId: 1,
-        productId: 2,
-        quantity: 3,
-      },
-    },
-    {
-      id: 4,
-      name: 'Brahma 600ml',
-      price: '7.50',
-      urlImage: 'http://localhost:3001/images/brahma_600ml.jpg',
-      SaleProduct: {
-        saleId: 1,
-        productId: 4,
-        quantity: 3,
-      },
-    },
-  ],
-  users: {
-    id: 3,
-    name: 'Cliente Zé Birita',
-    email: 'zebirita@email.com',
-    password: '1c37466c159755ce1fa181bd247cb925',
-    role: 'customer',
-  },
-  seller: {
-    id: 2,
-    name: 'Fulana Pereira',
-    email: 'fulana@deliveryapp.com',
-    password: '3c28d2b0881bf46457a853e0b07531c6',
-    role: 'seller',
-  },
+  products,
+  users,
+  seller,
 };
 
 export default { mockGetSalesOne,
